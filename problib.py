2c19aa94c65f11e417f9ef036b4913c101223b80
Stuart Wray <scw@stuartwray.net>
2013-04-10 15:02:30 +0100

    Probability: tank production example

---
+++ problib.py
@@ -0,0 +1,56 @@
+# Fiddle around with probabilities
+
+from math import *
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+        
+def log_factorial(x):
+    if x == 0:
+        return log(1)
+    else:
+        x = float(x)
+        return x * log(x)- x + log(2 * pi * x)/2 + 1/(12 * x) - 1/(360*x*x*x)
+
+def log_choice(n, k):
+    return log_factorial(n) - log_factorial(n - k) - log_factorial(k)
+
+def log_falling_power(n, k):
+    return log_factorial(n) - log_factorial(n - k)
+
+logZERO = -1.0e100 # set a floor to log probs in log_sum etc
+
+def log_sum(log_probs):
+    max_log_prob = max(log_probs)
+    s = sum(exp(x - max_log_prob) for x in log_probs)
+    if s == 0.0:
+        return logZERO
+    else:
+        return max_log_prob + log(s)
+
+log_product = sum # so our calculations look consistent
+  
+def log_normalise(log_probs):
+    log_total = log_sum(log_probs)
+    if log_total == logZERO:
+        return [log(1.0 / len(log_probs))] * len(log_probs) # hopefully not needed
+    else:
+        return [max(logZERO, log_p - log_total) for log_p in log_probs]
+
+def safe_log(x):
+    if x <= 0.0:
+        return logZERO
+    else:
+        return log(x)
+
+# for the birthday "paradox"
+# Assumes sampling with replacement (not strictly accurate)
+def prob_no_duplicate(items, samples):
+    log_top = log_factorial(items) - log_factorial(items - samples)
+    log_bottom = log(items) * samples
+
+    return exp(log_top - log_bottom)
+
---
+++ tank-production.py
@@ -0,0 +1,71 @@
+from problib import *
+
+# Five captured German tanks with these serial numbers:
+
+serials = [20, 31, 43, 78, 92]
+
+# According to Guardian ("Gavyn Davies does the maths, 20 July 2006), 
+# if    M = the highest observed serial,
+# and   S = number of serials,
+# then  E = (M - 1) * (S + 1) / S
+#       is a good estimator of the production run
+#       (ie max serial that could be observed)
+#       (in this case E = 109.2)
+
+# Let's see what Bayes thinks ...
+
+# P(H|D) = P(D|H) P(H|)
+#          ------------
+#             P(D|)
+#
+# Where P(D|) = sum over all h of P(D|h) P(h|)
+
+max_conceivable_number = 10000 # to pick something out of a hat
+log_probs = []
+
+for i in range(max_conceivable_number):
+    # Hypothesis H is "E = i"
+    log_P_H_given_nothing = log(1.0 / max_conceivable_number) # uniform prior
+
+    # Data is all the serials, which we can take sequentially
+    log_P_D_given_H = log(1.0) # for starters
+    for s, n in zip(serials, range(len(serials))):
+        if s > i:
+            # prob is zero! can't have serial greater than production run!
+            log_P_D_given_H += logZERO
+        else:
+            # uniform prob of picking any in this size production run,
+            # but need to account for the ones we already picked
+            left_in_run = i - n
+            log_P_D_given_H += log(1.0 / left_in_run) 
+
+    log_likelihood_D = log_P_D_given_H + log_P_H_given_nothing
+    log_probs.append(log_likelihood_D)
+    
+# Now need to divide all by P(D|), which is just normalisation ...
+
+log_probs = log_normalise(log_probs)
+
+# Note that this is the probability density function
+# The expected production size E comes at the median of this
+# so translate to cumulative frequency and plot
+
+cumulative = 0.0
+out = open("tmp.csv", "wb")
+
+for i, p in zip(range(len(log_probs)), log_probs):
+    if i > 200:
+        break
+    cumulative += exp(p)
+    text = "%d,%f" % (i, cumulative)
+    print text
+    print >>out, text
+
+
+out.close()
+
+# we get 108 as the median, and hence the best guess for the
+# production run. We can also easily read off lower and upper
+# quartiles, namely 98 and 128.
+
+
